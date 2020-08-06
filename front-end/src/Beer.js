8414153c73b88240968587e821f23cbe94598780
geo papa <cgeocodgod@gmail.com>
2020-08-06 15:50:16 +0300

    front-end update

--- front-end/src/Beer.js
+++ front-end/src/Beer.js
@@ -8,6 +8,7 @@ class Beer extends React.Component {
 
   constructor(props) {
     super(props);
+    this.setState({beer: props.beer});
     this.beer = props.beer;
   }
 
@@ -39,39 +40,39 @@ class Beer extends React.Component {
           <img src="https://illustoon.com/photo/3821.png" alt="Beer Img" width="300"></img>
           <div id="content">
             {this.isEditing
-              ? <input type="text" id="name" name="name" placeholder="Beer Name" onChange={this.changeHandler}></input>
+              ? <input type="text" id="name" name="name" placeholder="Beer Name" defaultValue={this.beer.name} onChange={this.changeHandler}></input>
               : <h1 id="name">{this.beer.name}</h1>
             }
             {this.isEditing
-              ? <input type="text" id="desc" name="description" placeholder="Beer Desrciption" value={this.beer.description}></input>
+              ? <input type="text" id="desc" name="description" placeholder="Beer Desrciption" defaultValue={this.beer.description} onChange={this.changeHandler}></input>
               : <p id="desc">{this.beer.description}</p>
             }
             <table>
               <tr>
                 <th>Bitterness</th>
                   {this.isEditing
-                    ? <td><input type="number" id="bitterness" name="bitterness" placeholder="Beer Bitterness" min="1" max="5" value={this.beer.bitterness}></input></td>
+                    ? <td><input type="number" id="bitterness" name="bitterness" placeholder="Beer Bitterness" min="1" max="5" defaultValue={this.beer.bitterness} onChange={this.changeHandler}></input></td>
                     : <td>{this.beer.bitterness}</td>
                   }
               </tr>
               <tr>
                 <th>Taste</th>
                   {this.isEditing
-                    ? <td><input type="number" id="taste" name="taste_intensity" placeholder="Beer Taste Intesity" min="1" max="5" value={this.beer.taste_intensity}></input></td>
+                    ? <td><input type="number" id="taste" name="taste_intensity" placeholder="Beer Taste Intesity" min="1" max="5" defaultValue={this.beer.taste_intensity} onChange={this.changeHandler}></input></td>
                     : <td>{this.beer.taste_intensity}</td>
                   }
               </tr>
               <tr>
                 <th>Foam</th>
                   {this.isEditing
-                    ? <td><input type="number" id="foam" name="foam_intensity" placeholder="Beer Foam Intesity" min="1" max="5" value={this.beer.foam_intensity}></input></td>
+                    ? <td><input type="number" id="foam" name="foam_intensity" placeholder="Beer Foam Intesity" min="1" max="5" defaultValue={this.beer.foam_intensity} onChange={this.changeHandler}></input></td>
                     : <td>{this.beer.foam_intensity}</td>
                   }
               </tr>
               <tr>
                 <th>CO2</th>
                  {this.isEditing
-                    ? <td><input type="number" id="co2" name="co2_feel" placeholder="Beer CO2 Feel" min="1" max="5" value={this.beer.co2_feel}></input></td>
+                    ? <td><input type="number" id="co2" name="co2_feel" placeholder="Beer CO2 Feel" min="1" max="5" defaultValue={this.beer.co2_feel} onChange={this.changeHandler}></input></td>
                     : <td>{this.beer.co2_feel}</td>
                   }
               </tr>
